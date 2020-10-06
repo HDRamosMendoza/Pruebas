8d7058b12b78f0a909b98118f24d60f9ba5b3afb
Daniel Ramos Mendoza <heber.daniel.ramos.mendoza@gmail.com>
2020-10-05 19:31:57 -0500

    Se limpia proyecto

--- MVCS_2/widgets/WidgetProyectos/nls/strings.js
+++
@@ -1,9 +0,0 @@
-define({
-  root: {
-    _widgetLabel: 'Widget Proyectos',
-    widgetTitle: 'Widget Proyectos',
-    description: 'A custom Web AppBuilder widget.'
-  }
-  // add supported locales below:
-  // , "zh-cn": true
-});
--- MVCS_2/widgets/WidgetProyectos3/nls/strings.js
+++
@@ -1,9 +0,0 @@
-define({
-  root: {
-    _widgetLabel: 'Widget Proyectos3',
-    widgetTitle: 'Widget Proyectos3',
-    description: 'A custom Web AppBuilder widget.'
-  }
-  // add supported locales below:
-  // , "zh-cn": true
-});
